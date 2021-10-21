0da4fc0374c6084e7175251963947a40c262c781
nextgeofront <nextgeofront@gmail.com>
2021-10-21 16:41:41 +0900

    first commit

--- wpsm/views.py
+++ wpsm/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
 @csrf_exempt
 def insert_fp(request):
-    return JsonResponse({})
+    data = json.loads(request.body)
+    return JsonResponse(data=data)
