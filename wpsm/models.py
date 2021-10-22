4be880157d12f6fcac5ba060a8a7790d7577b7d4
nextgeofront <nextgeofront@gmail.com>
2021-10-22 09:35:30 +0900

    first commit

--- wpsm/models.py
+++ wpsm/models.py
@@ -7,13 +7,17 @@ class FP(models.Model):
     longitude = models.FloatField('longitude')
     provider = models.CharField('provider', max_length=10)
     accuracy = models.FloatField('accuracy')
+
+    user_lat = models.FloatField('user latitude', null=True)
+    user_lng = models.FloatField('user longitude', null=True)
+
     app_name = models.CharField('app name that is gathering', max_length=50)
-    location_name = models.CharField(max_length=30)
+    position = models.CharField(max_length=30,null=True)
     reg_date = models.DateField('date published', default=datetime.today)
 
 
 class ScanMac(models.Model):
-    fp_id = models.ForeignKey(FP, on_delete=models.CASCADE)
+    fp = models.ForeignKey(FP, on_delete=models.CASCADE)
     ssid = models.CharField('ssid', max_length=32)
     mac = models.CharField('mac_address', max_length=17)
     rssi = models.SmallIntegerField('signal strength')
--- wpsm/views.py
+++ wpsm/views.py
@@ -2,9 +2,35 @@ from django.shortcuts import render
 from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+from .models import FP, ScanMac
 
 # Create your views here.
 @csrf_exempt
 def insert_fp(request):
     data = json.loads(request.body)
+    try:
+        fp = FP.objects.create(latitude=data['locations']['latitude'],
+           longitude=data['locations']['longitude'],
+           provider=data['locations']['provider'],
+           accuracy=data['locations']['accuracy'],
+           user_lat=data['device_info']['latitude'],
+           user_lng=data['device_info']['longitude'],
+           app_name=data['device_info']['package_name'],
+           position=data['device_info']['position'])
+        scan_macs = []
+        for scan in data['wifi_towers']:
+            scan_macs.append(ScanMac(fp_id=fp,
+                                     ssid=scan['ssid'],
+                                     mac=scan['mac_address'].upper(),
+                                     rssi=scan['signal_strength'],
+                                     freq=scan['frequency']))
+        ScanMac.objects.bulk_create(scan_macs)
+    except Exception as ex:
+        print(ex)
     return JsonResponse(data=data)
+
+@csrf_exempt
+def wps(request):
+    data = json.loads(request.body)
+    return JsonResponse({})
+
